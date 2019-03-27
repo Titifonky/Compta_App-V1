f09dcf59ddbb082ca225bdda7ea593cbc400d033
Etienne Canuel <etienne.canuel@gmail.com>
2019-03-27 22:46:33 +0100

    Maj

---
+++ Compta_App V1/Objets/Version.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Compta
+{
+    [ForcerAjout]
+    public class Version : ObjetGestion
+    {
+        public Version() { }
+
+        public Version(int No)
+        {
+            _No = No;
+        }
+
+        [Propriete, Tri]
+        public override int No
+        {
+            get { return _No; }
+            set { _No = value; }
+        }
+    }
+}
